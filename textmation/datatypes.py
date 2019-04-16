0908d58ed507fb40a13216f8acd0d6ff7cb7a9cf
Vallentin <vallentinsource@gmail.com>
2019-04-16 16:17:07 +0200

    Reworked rect type

--- textmation/datatypes.py
+++ textmation/datatypes.py
@@ -610,6 +610,103 @@ class Vec4(Value):
 		return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}, {self.w})"
 
 
+class _Rect(Type):
+	def __add__(self, other):
+		if other in (Vec2Type, NumberType):
+			return RectType
+		return NotImplemented
+
+	__radd__ = __add__
+	__sub__ = __add__
+	__rsub__ = __add__
+
+
+RectType = _Rect()
+
+
+class Point(Vec2):
+	pass
+
+
+class Size(Vec2):
+	@property
+	def width(self):
+		return self.x
+
+	@width.setter
+	def width(self, width):
+		self.x = width
+
+	@property
+	def height(self):
+		return self.y
+
+	@height.setter
+	def height(self, height):
+		self.y = height
+
+	@property
+	def area(self):
+		return self.width * self.height
+
+
+class Rect(Value):
+	type = RectType
+
+	def __init__(self, *rect):
+		assert 0 <= len(rect) <= 2
+
+		if len(rect) == 0:
+			position, size = Point(), Size()
+		elif len(rect) == 1:
+			position, size = Point(), Size(*rect[0])
+		else: # elif len(rect) == 2:
+			position, size = Point(*rect[0]), Size(*rect[1])
+
+		assert isinstance(position, Point)
+		assert isinstance(size, Size)
+
+		self.position, self.size = position, size
+
+	@property
+	def min(self):
+		return self.position
+
+	@property
+	def max(self):
+		return Point(*(self.position + self.size))
+
+	def __add__(self, other):
+		if isinstance(other, Vec2):
+			return Rect(self.position + other, self.size)
+		if isinstance(other, Number):
+			return self + Vec2(other.value)
+		if isinstance(other, (int, float)):
+			return self + Vec2(other)
+		return NotImplemented
+
+	__radd__ = __add__
+
+	def __sub__(self, other):
+		if isinstance(other, Vec2):
+			return Rect(self.position - other, self.size)
+		if isinstance(other, Number):
+			return self - Vec2(other.value)
+		if isinstance(other, (int, float)):
+			return self - Vec2(other)
+		return NotImplemented
+
+	def __rsub__(self, other):
+		if isinstance(other, Number):
+			return Rect(other - self.position, self.size)
+		if isinstance(other, (int, float)):
+			return Vec2(other) - self
+		return NotImplemented
+
+	def __repr__(self):
+		return f"{self.__class__.__name__}({self.position!r}, {self.size!r})"
+
+
 class Expression(Value):
 	def eval(self):
 		raise NotImplementedError
