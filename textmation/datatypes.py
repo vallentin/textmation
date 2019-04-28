74af6fde067c0b280bdc8c31d4602d438f83a4dd
Vallentin <vallentinsource@gmail.com>
2019-04-28 19:02:26 +0200

    Removed obsolete code

--- textmation/datatypes.py
+++ textmation/datatypes.py
@@ -6,43 +6,6 @@ from functools import total_ordering
 import math
 
 
-"""
-from .binding import Binding
-
-
-class Type(Binding):
-	@staticmethod
-	def check(value):
-		pass
-
-	def __init__(self, value):
-		self.value = value
-
-	def eval(self, property):
-		return self.value
-
-	def __repr__(self):
-		return f"<{self.__class__.__name__}: {self.value}>"
-
-
-class String(Type):
-	@staticmethod
-	def check(value):
-		assert isinstance(value, str)
-
-
-class Number(Type):
-	@staticmethod
-	def check(value):
-		assert isinstance(value, (int, float))
-
-
-class Percentage(Type):
-	def eval(self, property):
-		return self.value * (property.relative.eval() / 100)
-"""
-
-
 class Type:
 	@property
 	def name(self):
