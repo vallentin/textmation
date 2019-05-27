7787a3e37dc42c5d43676a6e5551f629ce084dff
Vallentin <vallentinsource@gmail.com>
2019-05-27 15:51:39 +0200

    Added default font downloading

--- textmation/prepare.py
+++ textmation/prepare.py
@@ -3,13 +3,17 @@
 
 import os
 from os.path import exists, isfile, join, dirname, abspath, relpath
+import re
+from zipfile import ZipFile
 
 from .elements import ElementError, Image
 from .webtools import *
 
 
-_cache_dir = abspath(join(dirname(__file__), os.pardir, ".cache"))
+_textmation_dir = abspath(join(dirname(__file__), os.pardir))
+_cache_dir = abspath(join(_textmation_dir, ".cache"))
 _images_dir = join(_cache_dir, "images")
+_fonts_dir = join(_textmation_dir, "fonts")
 
 
 def _download_image(url):
@@ -48,6 +52,43 @@ def _download_images(scene):
 			element.set("filename", _download_image(url))
 
 
+def _download_font(url):
+	if "fonts.google.com" in url:
+		font_name = re.search(r"\/([\w+]+)$", url)
+		assert font_name is not None
+		font_name = font_name.group(1)
+
+		font_name = font_name.replace("+", "%20")
+		url = f"https://fonts.google.com/download?family={font_name}"
+
+		filename = join(_fonts_dir, "font.zip")
+
+		print("Downloading:", url, "->", relpath(filename), flush=True)
+
+		os.makedirs(_fonts_dir, exist_ok=True)
+		download(url, filename)
+
+		print("Downloaded:", url, "->", relpath(filename), flush=True)
+		print("Unpacking", relpath(filename), flush=True)
+
+		with ZipFile(filename, "r") as zip:
+			zip.extractall(_fonts_dir)
+
+		print("Unpacked", relpath(filename), flush=True)
+		print("Removing", relpath(filename), flush=True)
+
+		os.remove(filename)
+	else:
+		raise Exception("Unsupported font service\nSupported: https://fonts.google.com")
+
+
+def _download_fonts(scene):
+	default_font = join(_fonts_dir, "Montserrat-Regular.ttf")
+	if not exists(default_font):
+		_download_font("https://fonts.google.com/specimen/Montserrat")
+
+
 def prepare(scene):
 	_download_images(scene)
+	_download_fonts(scene)
 	return scene
